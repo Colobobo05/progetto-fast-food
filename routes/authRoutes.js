3e54fa38642c6d4aab0179162d38ef319e7551bd
Colobobo05 <clmdvd05@gmail.com>
2026-08-28 22:44:56 +0200

    Delete routes directory

--- routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-// Definizione degli endpoint REST
-router.post('/register', authController.register);
-router.post('/login', authController.login);
-
-module.exports = router;
--- routes/searchRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const searchController = require('../controllers/searchController');
-
-// Endpoint per la ricerca 
-router.get('/restaurants', searchController.searchRestaurants);
-router.get('/dishes', searchController.searchDishes);
-
-module.exports = router;
